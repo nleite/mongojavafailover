ccf73b62d059ffcd2f4b4a585768664e17d9bc30
Norberto <git@norbertoleite.com>
2013-08-30 11:20:36 +0200

    adding test files

---
+++ src/test/java/mongodb/failover/FailoverTestTest.java
@@ -0,0 +1,24 @@
+package mongodb.failover;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class FailoverTestTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testFailoverTest() {
+		fail("Not yet implemented");
+	}
+
+}
